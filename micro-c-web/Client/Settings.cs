bbdc13f1ddbd9d33a384c499ffc1f25a4768cfff
Brian Barrett <blaxbb@gmail.com>
2021-02-10 02:13:42 -0500

    Categories for searching

--- micro-c-web/Client/Settings.cs
+++ micro-c-web/Client/Settings.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using static MicroCLib.Models.BuildComponent;
 
 namespace micro_c_web.Client
 {
@@ -12,9 +13,27 @@ namespace micro_c_web.Client
     {
         private string store;
         private float taxRate;
+        private List<ComponentType> categories;
+        private string categoriesPreset;
 
         public string Store { get => store; set => SetValue(ref store, value); }
         public float TaxRate { get => taxRate; set => SetValue(ref taxRate, value); }
+        public List<ComponentType> Categories { get => categories; set => SetValue(ref categories, value); }
+        public string CategoriesPreset { get => categoriesPreset; set => SetValue(ref categoriesPreset, value); }
+
+        public Settings()
+        {
+            Categories = new List<ComponentType>()
+            {
+                ComponentType.Desktop,
+                ComponentType.Laptop,
+                ComponentType.CPU,
+                ComponentType.GPU
+            };
+            CategoriesPreset = "Custom";
+            Store = "141";
+            TaxRate = .075f;
+        }
 
         public static async Task<Settings> Get(IJSRuntime js)
         {
@@ -35,7 +54,7 @@ namespace micro_c_web.Client
             OnPropertyChanged(propertyName);
         }
 
-        private void OnPropertyChanged(string name)
+        public void OnPropertyChanged(string name)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
--- micro-c-web/Client/wwwroot/js/Interop.js
+++ micro-c-web/Client/wwwroot/js/Interop.js
@@ -29,3 +29,34 @@ window.SetLocalStorage = function (key, value) {
 window.GetLocalStorage = function (key) {
     return localStorage.getItem(key);
 }
+
+window.DestroySortable = function (selector) {
+    if (window.sortable !== undefined) {
+        window.sortable.destroy();
+    }
+    $(selector).empty();
+}
+
+window.CreateSortable = function (selector, categories, settingsPage) {
+    if (window.sortable !== undefined) {
+        window.sortable.destroy();
+    }
+
+    $(selector).replaceWith("<ul class='list-group' id='settings-category-items'></ul>");
+    categories.forEach(cat => $(selector).append("<li class='list-group-item'>" + cat + "</li>"));
+
+    var obj = $(selector).get(0);
+    window.sortable = Sortable.create(obj, {
+        animation: 100,
+        onEnd: function (evt) {
+            if (evt.oldIndex != evt.newIndex) {
+                settingsPage.invokeMethodAsync("CategoryIndexChanged", evt.oldIndex, evt.newIndex);
+            }
+            return false;
+        }
+    });
+}
+
+window.SetSelectValue = function(id, value) {
+    $(id).val(value);
+}
