597d7029f47c146afd8690b16b3d837288ea1daf
Brian Barrett <blaxbb@gmail.com>
2021-03-16 00:59:17 -0400

    add db migrations for picture url

---
+++ micro-c-web/Server/Migrations/20210316044459_picture urls.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace micro_c_web.Server.Migrations
+{
+    public partial class pictureurls : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PictureUrls",
+                table: "ItemCache",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PictureUrls",
+                table: "ItemCache");
+        }
+    }
+}
