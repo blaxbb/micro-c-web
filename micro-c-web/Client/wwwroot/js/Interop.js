1e457c1b50533f67f7abbb6236e799515e471711
Brian Barrett <blaxbb@gmail.com>
2021-05-08 05:09:08 -0400

    Focus search box on quote add item, fix single search result not invoking action callback

--- micro-c-web/Client/wwwroot/js/Interop.js
+++ micro-c-web/Client/wwwroot/js/Interop.js
@@ -15,6 +15,19 @@ window.ShowModal = function (id) {
     $(id).modal();
 }
 
+let focusHandlers = {};
+
+window.ShowModalFocus = function (modal, focus) {
+    if (focusHandlers[modal] === undefined) {
+        focusHandlers[modal] = true;
+        $(modal).on('shown.bs.modal', function () {
+            console.log(focus);
+            $(focus).trigger('focus');
+        });
+    }
+    $(modal).modal();
+}
+
 window.HideModal = function (id) {
     $(id).modal('hide');
 }
