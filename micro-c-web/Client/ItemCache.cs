d38c6f64b988ccbf2459bb0eb94cef641b588ecd
Brian Barrett <blaxbb@gmail.com>
2021-05-16 02:49:04 -0400

    work on indexeddb

---
+++ micro-c-web/Client/ItemCache.cs
@@ -0,0 +1,46 @@
+﻿using MicroCLib.Models;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace micro_c_web.Client
+{
+    public class ItemCache
+    {
+        public Item Item { get; set; }
+        public string Category { get; set; }
+        public string SKU { get; set; }
+        public DateTime Created { get; set; }
+        public ItemCache()
+        {
+            Created = DateTime.Now;
+        }
+
+        public ItemCache(Item item)
+        {
+            Created = DateTime.Now;
+            Item = item;
+            SKU = item.SKU;
+            var cat = item.Categories?.FirstOrDefault();
+            Category = cat?.Filter ?? "";
+        }
+
+        public async Task Cache(IJSRuntime js)
+        {
+            Created = DateTime.Now;
+            await js.InvokeVoidAsync("AddCacheItem", this);
+        }
+
+        public static async Task<ItemCache> GetFromSKU(IJSRuntime js, string sku)
+        {
+            return await js.InvokeAsync<ItemCache>("GetCacheItem", sku);
+        }
+
+        public static async Task<ItemCache[]> GetFromCategory(IJSRuntime js, string category)
+        {
+            return await js.InvokeAsync<ItemCache[]>("GetCacheCategory", category);
+        }
+    }
+}
---
+++ micro-c-web/Client/wwwroot/js/Database.js
@@ -0,0 +1,31 @@
+﻿const db = idb.openDB("micro-c-cache", 3, {
+    upgrade(db, oldVersion, newVersion, transaction) {
+        console.log("upgrade");
+        var store = db.createObjectStore("items", { keyPath: "sku" });
+        store.createIndex("Name", "name", { unique: false });
+        store.createIndex("Category", "category", { unique: false })
+    },
+    blocked() {
+
+    },
+    blocking() {
+
+    },
+    terminated() {
+
+    }
+});
+
+async function AddCacheItem(item) {
+    (await db).add("items", item);
+}
+
+async function GetCacheItem(sku) {
+    return (await db).get('items', sku);
+}
+
+async function GetCacheCategory(category) {
+    var res = await (await db).getAllFromIndex("items", "Category", category);
+    debugger;
+    return res;
+}
