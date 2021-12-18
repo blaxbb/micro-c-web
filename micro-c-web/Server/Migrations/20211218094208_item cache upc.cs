872b2c9efccb4fa664ec7b4f7ee8713dc0cd2d06
Brian Barrett <blaxbb@gmail.com>
2021-12-18 05:08:06 -0500

    Added missing db migrations

---
+++ micro-c-web/Server/Migrations/20211218094208_item cache upc.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace micro_c_web.Server.Migrations
+{
+    public partial class itemcacheupc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SKU",
+                table: "ItemCache",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UPC",
+                table: "ItemCache",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemCache_SKU",
+                table: "ItemCache",
+                column: "SKU");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemCache_UPC",
+                table: "ItemCache",
+                column: "UPC");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ItemCache_SKU",
+                table: "ItemCache");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ItemCache_UPC",
+                table: "ItemCache");
+
+            migrationBuilder.DropColumn(
+                name: "UPC",
+                table: "ItemCache");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SKU",
+                table: "ItemCache",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+    }
+}
